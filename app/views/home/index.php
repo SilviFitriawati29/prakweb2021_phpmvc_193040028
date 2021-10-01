4df0bfdc2951592919c7c03f0faea52a60334611
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-10-01 09:02:17 +0700

    Membuat sebuah Assets sesuai dengan Video ke 6

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,11 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+  <div class="jumbotron mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Ini merupakan website saya</p>
+    <hr class="my-2">
+    <p>More info</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Learn More</a>
+    </p>
+  </div>
+</div> 
